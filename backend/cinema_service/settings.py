ce685890bb404794e976980160cc66d5b881e593
Gibbs <mr.darmstadtiumgmail.com>
2024-09-13 10:08:48 +0200

    Return .env.sample and STATIC and MEDIA ROOT settings

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -137,11 +137,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = "/vol/web/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
